ed69e0a3c2a4eb131e8dee89e9b2de27fbdb7193
JAMES MBUVI <completed assignment>
2023-12-22 21:13:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+describe("what-is-a-test", () => {
+  describe("Name", () => {
+    it("returns 'Susan'", () => {
+      // Define the name
+      var name = "Susan";
+      expect(name).toBe("Susan");
+    });
+  });
 
-module.exports = {name, height, message};
+  describe("Height", () => {
+    it("is less than 40", () => {
+      // Define the height
+      var height = 35;
+      expect(height).toBeLessThan(40);
+    });
+
+    it("is greater than 0", () => {
+      // Define the height
+      var height = 35;
+      expect(height).toBeGreaterThan(0);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      // Define the name and height
+      var name = "Susan";
+      var height = 35;
+      
+      // Generate the message
+      var message = `Name\n 2) returns "${name}"\nHeight\n 3) is less than 40 and greater than 0\nMessage\n 4) gives the name and height`;
+
+      expect(message).toBeTruthy();
+    });
+  });
+});
